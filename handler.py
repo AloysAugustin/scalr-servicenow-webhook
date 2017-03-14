fb188de519e385cf2e6d61c2bdfd6b0f5b5ccfd0
Mohammed Hawari <mohammed@hawari.fr>
2017-03-14 12:37:53 +0100

    Initial commit

---
+++ handler.py
@@ -0,0 +1,60 @@
+SCALR_SIGNING_KEY = 'enter-your-scalr-signing-key-here' 
+USERNAME = 'enter-your-servicenow-username-here' 
+PASSWORD = 'enter-your-servicenow-password-here' 
+URL = 'https://enter-your-servicenow-domain-here/api/now/table/u_scalr_events' 
+
+import urllib2 
+import json 
+import hmac 
+import binascii 
+import dateutil.parser 
+from hashlib import sha1 
+from datetime import tzinfo, timedelta, datetime 
+
+ZERO = timedelta(0) 
+
+class UTC(tzinfo):
+    def utcoffset(self, dt):
+        return ZERO
+    def tzname(self, dt):
+        return "UTC"
+    def dst(self, dt):
+        return ZERO 
+
+utc = UTC() 
+
+@app.route("/servicenow/")
+def lambda_handler():
+
+    if not validateRequest(request):
+        return 'SIGNATURE ERROR'
+    body = json.loads(request.data)
+    
+    values = {'u_event': body['eventName'], 'u_eventid': body['eventId'], 'u_payload': request.data }
+    
+    headers = {'Content-Type': 'application/json'}
+    
+    data = json.dumps(values).encode('utf8')
+    password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
+    password_mgr.add_password(None, URL, USERNAME, PASSWORD)
+    handler = urllib2.HTTPBasicAuthHandler(password_mgr)
+    opener = urllib2.build_opener(handler)
+    urllib2.install_opener(opener)
+    req = urllib2.Request(URL, data, headers)
+    response = urllib2.urlopen(req)
+    
+    return response.read()
+    
+def validateRequest(request):
+    if not 'X-Signature' in request.headers or not 'Date' in request.headers:
+        return False
+    date = request.headers['Date']
+    body = request.data
+    expected_signature = binascii.hexlify(hmac.new(SCALR_SIGNING_KEY, body + date, sha1).digest())
+    if expected_signature != request.headers['X-Signature']:
+        return False
+        
+    date = dateutil.parser.parse(date)
+    now = datetime.now(utc)
+    delta = abs((now - date).total_seconds())
+    return delta < 300
