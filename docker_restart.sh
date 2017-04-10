b8c3fab58f6f76c48d7c857437fe7fa6a139a566
Aloys Augustin <aloys.augustin@polytechnique.org>
2017-04-10 16:06:54 +0200

    Always restart container

--- docker_restart.sh
+++ docker_restart.sh
@@ -3,4 +3,4 @@
 docker stop webhook-servicenow
 docker rm webhook-servicenow
 docker build -t webhook-servicenow .
-docker run -p 5000:5000 -tid --name webhook-servicenow webhook-servicenow
+docker run -p 5000:5000 -tid --restart always --name webhook-servicenow webhook-servicenow
